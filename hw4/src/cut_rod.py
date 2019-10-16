3db78ae3169438b488e037aa3b18d2751938db1b
wchstrife <wch960426@126.com>
2019-10-16 21:42:56 +0800

    hw4-切割钢条

--- hw4/src/cut_rod.py
+++ hw4/src/cut_rod.py
@@ -26,6 +26,35 @@ def Memoized_Cut_Rod_Aux(p, n, r):
     
     return max_value
 
+# 动态规划-带备忘录的自顶向下方法-记录路径
+def Extended_Memoized_Cut_Rod(p, n):
+    r = [-1] * (n + 1)
+    s = [0] * (n + 1)  # 记录最优解路径
+    max_value = Extended_Memoized_Cut_Rod_Aux(p, n, r, s)
+    print(r)
+    print(s)
+    print("最优解：%d" % r[n])
+    while n > 0:
+        print(s[n])
+        n = n - s[n]
+
+def Extended_Memoized_Cut_Rod_Aux(p, n, r, s):
+    if r[n] >= 0:
+        return (r[n])
+    if n == 0:
+        max_value = 0
+    else:
+        max_value = -1
+    for i in range(1, n+1):
+        temp = Extended_Memoized_Cut_Rod_Aux(p, n-i, r, s)
+        if max_value < p[i] + temp:
+            max_value = p[i] + temp
+            s[n] = i    # 记录最优时切分点的位置
+    r[n] = max_value    # 记录下n时的最小值
+    
+    return max_value
+
+
 # 动态规划-自底向上
 def Buttom_Up_Cut_Rod(p, n):
     r = [-1] * (n + 1)
@@ -67,3 +96,4 @@ if __name__ == "__main__":
     # print(Memoized_Cut_Rod(p, 9))
     # print(Buttom_Up_Cut_Rod(p, 9))
     Print_Cut_Rod_Solution(p, 10)
+    Extended_Memoized_Cut_Rod(p, 10)
